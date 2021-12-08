82ed2fe5c089034c2648da12111ea0ef04d37840
AKKA\Jan.WEISS <jan.weiss@akka.eu>
2021-12-08 09:20:42 +0100

    Finish exercise of day 8

--- src/day_08.rs
+++ src/day_08.rs
@@ -1,12 +1,12 @@
 use ansi_term::Color::{Red, RGB};
-use std::{fs, collections::HashMap};
+use std::{collections::HashMap, fs};
 
 pub fn day_08_main() {
     println!(
-        "\n{}\n\t• 1 Fuel required by crabs: {}\n\t• n Fuel required by crabs: {}",
+        "\n{}\n\t• Appearances of 1, 4, 7, 8: {}\n\t• Sum of all outcomes: {}",
         RGB(204, 204, 0)
             .underline()
-            .paint("These are the results for day 7:"),
+            .paint("These are the results for day 8:"),
         Red.paint(first_part_08().to_string()),
         Red.paint(second_part_08().to_string())
     )
@@ -20,8 +20,12 @@ fn first_part_08() -> i32 {
         for (index, digit) in digits.split_terminator(" | ").into_iter().enumerate() {
             if index == 1 {
                 for signal in digit.split_whitespace() {
-                    if signal.len() == 2 || signal.len() == 3 || signal.len() == 4 || signal.len() == 7 { 
-                        digit_vec.push(signal); 
+                    if signal.len() == 2
+                        || signal.len() == 3
+                        || signal.len() == 4
+                        || signal.len() == 7
+                    {
+                        digit_vec.push(signal);
                     }
                 }
             }
@@ -31,17 +35,18 @@ fn first_part_08() -> i32 {
     digit_vec.len() as i32
 }
 
-fn second_part_08() -> i64 {
+fn second_part_08() -> i32 {
     let digit_string = fs::read_to_string("assets/08/seven_segment.txt").unwrap();
-    // let mut digit_vec = Vec::new();
+    let mut digit_vec = Vec::new();
 
     for digits in digit_string.lines() {
-        let test: Vec<&str> = digits.split_terminator(" | ").collect(); 
+        let test: Vec<&str> = digits.split_terminator(" | ").collect();
         let mut result = String::new();
         let temp = &search_numbers(test[0].trim());
 
         for signal in test[1].split_whitespace() {
-            for (key, value) in temp {
+            let signal = signal.trim();
+            for (index, (key, value)) in temp.into_iter().enumerate() {
                 let mut counter = 0;
                 for ch in signal.chars() {
                     if value.contains(ch) {
@@ -49,36 +54,43 @@ fn second_part_08() -> i64 {
                     }
                 }
 
-                if counter == value.len() && counter == signal.len() {
+                if counter == value.len() && value.len() == signal.len() {
                     result.push_str(&(key.to_string()));
                 }
-            println!("SIGNAL {}", result);
+
+                if result.len() == 4 && index == (temp.len() - 1) {
+                    digit_vec.push(result.trim().parse::<i32>().unwrap());
+                }
             }
         }
     }
-    
-    4
+
+    let mut result_counter = 0;
+    for digit in digit_vec {
+        result_counter += digit;
+    }
+
+    result_counter
 }
 
 fn search_numbers(digit_input: &str) -> HashMap<i32, &str> {
     let mut result = HashMap::new();
 
     while result.len() < 10 {
-
         for digit in digit_input.split_whitespace() {
             match digit.len() {
                 2 => {
-                    result.entry(1).or_insert( digit);
-                },
+                    result.entry(1).or_insert(digit);
+                }
                 3 => {
-                    result.entry(7).or_insert( digit);
-                },
+                    result.entry(7).or_insert(digit);
+                }
                 4 => {
-                    result.entry(4).or_insert( digit);
-                },
+                    result.entry(4).or_insert(digit);
+                }
                 7 => {
-                    result.entry(8).or_insert( digit);
-                },
+                    result.entry(8).or_insert(digit);
+                }
                 6 => {
                     let mut counter = 0;
                     for ch in digit.chars() {
@@ -100,13 +112,12 @@ fn search_numbers(digit_input: &str) -> HashMap<i32, &str> {
                         }
 
                         if counter == 4 {
-                            result.entry(9).or_insert(digit);
+                            *result.entry(9).or_insert(digit) = digit;
                         } else {
-                            result.entry(0).or_insert(digit);
+                            *result.entry(0).or_insert(digit) = digit;
                         }
-                        
-                    } 
-                },
+                    }
+                }
                 5 => {
                     let mut counter = 0;
                     for ch in digit.chars() {
@@ -128,13 +139,12 @@ fn search_numbers(digit_input: &str) -> HashMap<i32, &str> {
                         }
 
                         if counter == 4 {
-                            result.entry(2).or_insert(digit);
+                            *result.entry(2).or_insert(digit) = digit;
                         } else {
-                            result.entry(5).or_insert(digit);
+                            *result.entry(5).or_insert(digit) = digit;
                         }
                     }
-
-                },
+                }
                 _ => println!("Unknown pattern occured"),
             }
         }
